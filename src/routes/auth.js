65c3ca3036cff405ceae7e75a046381a2142f8e8
Onur Kapucu <kapucuonur@hotmail.comcat>
2024-10-14 09:09:58 +0300

    env

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,6 @@
 const router = require('express').Router(); 
 const authContrller = require('../controller/authController');
 
-router.post('/', authContrller.postUser);
+router.post('/', authController.postUser);
 
 module.exports = router;
